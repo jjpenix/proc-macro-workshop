80cb24cdaa0c94e92b42abd77e89b9d5bf8503d4
jp <jp>
2020-10-25 18:09:25 -0400

    test 2 passing

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,57 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    // fetch name, make name + Builder version
+    let input_ident = &input.ident;
+    let builder_name = format!("{}Builder", input_ident);
+    let builder_ident = syn::Ident::new(&builder_name, input_ident.span());
+
+    // get list of fields in the input struct
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = input.data
+    {
+        named
+    } else {
+        panic!("Non-struct found!");
+    };
+
+    let builder_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! {
+            #name: std::option::Option<#ty>
+        }
+    });
+
+    let builder_empty_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        quote! {
+            #name: None
+        }
+    });
+
+    let expanded = quote! {
+        pub struct #builder_ident {
+            #(#builder_fields,)*
+        }
+
+        impl #input_ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    #(#builder_empty_fields,)*
+                }
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
 }
